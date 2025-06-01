544348a7ef8b5f0636b9b7bfd0b39a2a5a8252a2
Kook-s <kooks1126@naver.com>
2025-06-01 18:13:00 +0900

    session03

---
+++ src/main/kotlin/Lec05Main.kt
@@ -0,0 +1,62 @@
+fun judgeNumber(number: Int) {
+    when (number) {
+        1, 0, -1 -> print("10-1")
+        else -> print("아무것도 아님")
+    }
+}
+
+fun startWithA(obj: Any): Boolean {
+    return when (obj) {
+        is String -> obj.startsWith("A")
+        else -> false
+    }
+}
+
+fun validateScoreIsNotNegative(score : Int) {
+    if (score in 0..100) {
+        throw IllegalArgumentException("${score}범위는 0부터 100이다.")
+    }
+}
+
+fun getGradeWithSwitch(score : Int): String {
+    return when (score / 10) {
+        9-> "A"
+        8-> "B"
+        7-> "C"
+        else -> "D"
+    }
+
+    return when (score) {
+        in 90..99-> "A"
+        in 80..89-> "B"
+        in 70..79-> "C"
+        else -> "D"
+    }
+
+}
+
+fun getPassOrFail(score : Int) : String{
+//    if (score > 50) {
+//        return "P"
+//    } else{
+//        return "F"
+//    }
+
+    return if (score > 50) {
+        "P"
+    } else{
+        "F"
+    }
+}
+
+fun getGrade(score : Int) : String{
+    return if (score >= 90) {
+        "A"
+    } else if (score >= 80) {
+        "B"
+    }else if (score >= 80) {
+        "D"
+    }else{
+        "C"
+    }
+}
---
+++ src/main/kotlin/Lec08Main.kt
@@ -0,0 +1,40 @@
+fun main() {
+    repeat("hello world")
+
+    printNameAndGender(gender = "FEMALE", name = "개발자")
+}
+
+fun max(a : Int , b : Int) : Int{
+    return if (a > b) {
+        a
+    } else {
+        b
+    }
+}
+
+fun repeat(str : String, num : Int = 3, userNewLine : Boolean = true){
+    for (i in 1..num) {
+        if (userNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+
+    }
+}
+
+fun printNameAndGender(name : String, gender : String){
+    println(name)
+    println(gender)
+}
+
+
+// printAll("A", "B", "C")
+// val array = arrayOf("A", "B", "C")
+// printAll(*array)
+
+fun printAll(vararg strings: String) {
+    for (str in strings) {
+        println(str)
+    }
+}
